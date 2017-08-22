6eaa9c007c4855b3f5faeb31f839c79b158536b0
nicholae <nicholae@lab1-13.eng.utah.edu>
2017-08-21 22:50:29 -0600

    Beginning of Matchlab

---
+++ matchlab.c
@@ -0,0 +1,20 @@
+
+typedef bool;
+#define true 1
+#define false 0
+
+//Matchlab
+
+int main(int argc, char* argv[])
+{
+
+	/// Check number of Flag arguments
+
+		//Case 1: 2 Flag Arguments
+
+		//Case 2: 3 Flag Arguments
+
+
+
+}
+
