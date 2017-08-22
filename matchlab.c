0b3b26e3702e06b1479a9f8c02817a9add8b21e5
nicholae <nicholae@lab1-13.eng.utah.edu>
2017-08-21 23:13:45 -0600

    If statements and ASCII

--- matchlab.c
+++ matchlab.c
@@ -5,14 +5,39 @@ typedef bool;
 
 //Matchlab
 
+#include <stdio.h>
+
 int main(int argc, char* argv[])
 {
 
 	/// Check number of Flag arguments
 
-		//Case 1: 2 Flag Arguments
+		/*
 
-		//Case 2: 3 Flag Arguments
+		- 45
+		a 97
+		b 98
+		c 99
+		t 116
+
+		*/
+
+
+		//Case 1: 2 Flag Arguments
+		if(argc < 3) // No -t flag
+		{
+
+
+
+		}else if(argc == 3)  //Case 2: 3 Flag Arguments
+		{
+
+
+
+		}	
+
+		return 0;
+
 
 
 
