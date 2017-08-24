9030387acd2e492f9f56541df44d8da3358b3605
nicholae <nicholae@lab1-7.eng.utah.edu>
2017-08-24 07:25:13 -0600

    more stuff

--- matchlab.c
+++ matchlab.c
@@ -7,16 +7,26 @@
 //Matchlab
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {A, B, C, T, CMD_ARG, ERROR_ARG } arg_type;
 
 void basicPrint(char arr[]);
+arg_type identifyArgType(char* cmdArg);
+
 
 int main(int argc, char* argv[])
 {
 
+
+		int x = 3;
 	/// Check number of Flag arguments
+		fprintf(stderr, "Billy");
+		printf("Billy%d", x);
+
 
 		/*
-
+	
 		- 45
 		a 97
 		b 98
@@ -24,29 +34,88 @@ int main(int argc, char* argv[])
 		t 116
 
 		*/
-
-
-		//Case 1: 2 Flag Arguments
-		if(argc < 3) // No -t flag
+		int i;
+		for(i = 1; i < argc; i++)
 		{
-			basicPrint(argv[0]);			
-			basicPrint(argv[1]);			
-			//basicPrint(argv[2]);
+			fprintf(stderr, "Wow");
+			arg_type ID = identifyArgType(argv[i]);
+			if(ID == A)
+			{
+				fprintf(stderr, "A\n");
+			}else if(ID == B)
+			{
+				fprintf(stderr, "B\n");
+
+			}else if(ID == C)
+			{
+				fprintf(stderr, "C\n");
+
+			}else if(ID == T)
+			{
+
+				fprintf(stderr, "T\n");
+			}else if(ID == CMD_ARG)
+			{
+				fprintf(stderr, "CMD_ARG\n");
+			}else
+			{
+				fprintf(stderr, "Error\n");
+			}
 
 
-		}else if(argc == 3)  //Case 2: 3 Flag Arguments
-		{
-
-
-
-		}	
+		}
 
 		return 0;
+}
 
 
+/*
+	Returns an enum indicating the type of flag
+*/
+arg_type identifyArgType(char* cmdArg)
+{
 
+
+	if(cmdArg[0] == '\0')
+	{
+
+		fprintf(stderr, "Error, null character");
+
+		return ERROR_ARG;
+
+	} 
+	
+
+	if(cmdArg[0] != '-')
+	{
+
+		return CMD_ARG;
+
+	}else if(cmdArg[0] == '-' && cmdArg[1] == 'a')
+	{
+
+		return A;
+
+	}else if(*cmdArg == '-' && *(cmdArg +1) == 'b')
+	{
+
+		return B;
+	}else if(*cmdArg == '-' && *(cmdArg +1) == 'c')
+	{
+
+		return C;
+	}else if(*cmdArg == '-' && *(cmdArg +1) == 't')
+	{
+
+		return T;
+	}else
+	{
+		return CMD_ARG;
+	}
 
 }
+
+
 
 void basicPrint(char* arr)
 {
